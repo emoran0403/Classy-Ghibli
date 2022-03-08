f0df687b5006426c7e14f2708d5d0d1fdc789269
Eric Moran <emoran0403@gmail.com>
2022-03-08 10:41:16 -0500

    imported bootstrap, set up basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CharacterDetails from "./components/CharacterDetails";
+import Characters from "./components/Characters";
+import FilmDetails from "./components/FilmDetails";
+import Films from "./components/Films";
+import Home from "./components/Home";
+import LocationDetails from "./components/LocationDetails";
+import Locations from "./components/Locations";
+import Navbar from "./components/Navbar";
 
 class App extends React.Component {
   constructor(props) {
@@ -7,7 +18,18 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <div>some text here</div>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/films" component={Films} />
+            <Route exact path="/characters" component={Characters} />
+            <Route exact path="/locations" component={Locations} />
+            <Route exact path="/films/:filmid" component={FilmDetails} />
+            <Route exact path="/characters/:characterid" component={CharacterDetails} />
+            <Route exact path="/locations/:locationid" component={LocationDetails} />
+          </Switch>
+        </BrowserRouter>
       </>
     );
   }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,11 @@ class Home extends React.Component {
   }
 
   render() {
-    return <div>this is home</div>;
+    return (
+      <div className="d-flex mt-4 justify-content-center">
+        <img src="Studio_Ghibli_Logo.jpg" alt="Studio Gibbles" width={1200} height="auto" />
+      </div>
+    );
   }
 }
 
